53322dca17afe901304ba17e245bb9cbb4afd6e1
jhoisz <jvrda@ufpi.edu.br>
2022-11-01 17:12:19 -0300

    #3 Adicionando lista de colaboradores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+    console.log(colaboradores)
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => { aoColaboradorAdicionado(colaborador) }} />
     </div>
   );
 }
